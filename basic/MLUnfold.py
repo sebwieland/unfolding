491b42e9ee29bed9623fa158c505afd1bb745ec3
Sebastian Wieland <swieland@ekplx80.physik.uni-karlsruhe.de>
2018-03-23 11:53:25 +0100

    small changes

--- basic/MLUnfold.py
+++ basic/MLUnfold.py
@@ -1,17 +1,11 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 from __future__ import print_function
 from root_numpy import root2array, tree2array
 import ROOT
 
 
-# In[2]:
 
 
+#Get Data
 rfile = ROOT.TFile("TreeFile.root")
 intree = rfile.Get('Tree')
 intree.Print()
@@ -19,83 +13,68 @@ intree.Print()
 array = tree2array(intree,branches=["Gen","Reco","data"])
 array.dtype.names = ('reco', "gen", "data")
 
-print(array)
-
-
-# In[3]:
-
-
+#Plot Data
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import numpy as np
 import pandas as pd
 df = pd.DataFrame(array)
 
-#print(df)
-content_reco ,bin,patches = plt.hist(df['reco'], bins=15)
+NBins=10
+xmin=0
+xmax=1000
+content_reco ,bin,patches = plt.hist(df['reco'], bins=NBins)
 plt.ylabel('# events')
 plt.xlabel("reco")
-plt.show()
+# plt.show()
+plt.savefig("plots/reco.pdf")
 
-content_gen ,bin,patches = plt.hist(df['gen'], bins=15)
+content_gen ,bin,patches = plt.hist(df['gen'], bins=NBins)
 print(content_gen)
 plt.ylabel('# events')
 plt.xlabel("gen")
-plt.show()
+plt.savefig("plots/gen.pdf")
+# plt.show()
 
-plt.hist2d(df['reco'],df['gen'], bins=10, range=[[0, 1000], [0, 1000]],norm=LogNorm())
+plt.hist2d(df['reco'],df['gen'], bins=NBins, range=[[xmin, xmax], [xmin, xmax]],norm=LogNorm())
 plt.ylabel('# events')
 plt.colorbar()
-plt.show()
+plt.savefig("plots/data.pdf")
+# plt.show()
 
 
-
-# In[4]:
-
-
-batch_size = 1000
-num_classes = 20
-NBins = num_classes
-epochs = 20
-
-
-# In[5]:
-
+#Do unfolding using NN
+batch_size = 500
+num_classes = NBins
+epochs = 10
 
 import keras
-gen=df["gen"].values
-print(gen)
-print(gen.shape)
-g=np.trunc(gen/200)
-print(g)
-reco=df["reco"].values
-print(reco)
-print(reco.shape)
-r=np.trunc(reco/200)
-print(r)
-data=df["data"].values
-data=np.trunc(data/200)
-data = data.reshape(data.shape[0],1,1)
-gcat = keras.utils.to_categorical(g,NBins)
-from sklearn.model_selection import train_test_split
-r = r.reshape(r.shape[0],1,1)
-r_train, r_test, g_train, g_test = train_test_split(r, gcat, test_size=0.22, random_state=42)
-
-
-# In[6]:
-
-
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.models import Sequential
 from keras.layers import Dense,Flatten
-from keras import backend as K
+
+gen=df["gen"].values
+# print(gen)
+# print(gen.shape)
+binwidth=(xmax-xmin)/NBins+1
+g=np.trunc(gen/binwidth)
+# print(g)
+reco=df["reco"].values
+# print(reco)
+# print(reco.shape)
+r=np.trunc(reco/binwidth)
+print(r)
+data=df["data"].values
+data=np.trunc(data/binwidth)
+data = data.reshape(data.shape[0],1,1)
+gcat = keras.utils.to_categorical(g,NBins)
+from sklearn.model_selection import train_test_split
+r = r.reshape(r.shape[0],1,1)
+r_train, r_test, g_train, g_test = train_test_split(r, gcat, test_size=0.2, random_state=42)
 
 
-# In[ ]:
-
-
-def prepareModel(nvar=1, NBins=10, kappa=8):
+def prepareModel(nvar=1, NBins=NBins, kappa=8):
     ''' Prepare KERAS-based sequential neural network with for ML unfolding. 
         Nvar defines number of inputvariables. NBins is number of truth bins. 
         kappa is an empirically tuned parameter for the intermediate layer'''
@@ -115,34 +94,27 @@ def prepareModel(nvar=1, NBins=10, kappa=8):
 model = prepareModel(1,NBins)
 
 
-# In[ ]:
-
 
 CallBack=keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=32, write_graph=True, write_grads=False, write_images=False, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None)
 
 h = model.fit(r_train,g_train,batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(r_test,g_test), callbacks=[CallBack])
 
 
-# In[ ]:
-
 
 # save model,if needed
 model.save("model.hdf5")
 
 
-# In[ ]:
+
 
 
 loss_and_metrics = model.evaluate(r_test, g_test, batch_size=128)
 print(loss_and_metrics)
 
 
-# In[ ]:
-
-
 prob = model.predict(r_test, batch_size=128)
 classes=prob.argmax(axis=-1)
 np.set_printoptions(threshold=100000)
-for i in range(20):    
+for i in range(40):    
     print(classes[i],g_test.argmax(axis=-1)[i])
 
